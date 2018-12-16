98c27eb925d679837cba799cf4a9997b1f13b32c
Rovexx <roel.versteeg@hotmail.com>
2018-12-16 15:58:50 +0100

    test options 1

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -11,6 +11,20 @@ var routes = function(Product){
 
     // middleware
     productRouter.use('/:productId', function(req, res, next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+        //intercepts OPTIONS method
+        if ('OPTIONS' === req.method) {
+            //respond with 200
+            res.send(200);
+        }
+        else {
+        //move on
+            next();
+        }
+
         Product.findById(req.params.productId, function(err, product){
             if(err)
                 res.status(500).send(err);
